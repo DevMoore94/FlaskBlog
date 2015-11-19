10a2d263ff43ab0eace64911409a4dda09d5c7b3
John Tramley <jtramley@gc127m02.cs.unb.ca>
2015-11-19 14:05:58 -0400

    added some mysql code and new route

--- blog.py
+++ blog.py
@@ -6,12 +6,25 @@ app = Flask(__name__)
 #create a temporary users table
 users = {"jtramley":"password"}
 
+#MySQL connection information.
+#host='localhost',user='jtramley',passwd='ckd9OY5fz',db='jtramley'
+hosta='localhost'
+usera='jtramley'
+passwda='ckd9OY5fz'
+dba='jtramley'
+
+
 #setup authentication process
 auth = HTTPBasicAuth()
 
 @auth.get_password
 def get_password(username):
 	#if the username is in the users table, return the password.
+	connection = MySQLdb.connect(host=hosta,user=usera,passwd=passwda,db=dba)
+	cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+
+	query = "select Password from users where Username='%s'" %(username)
+
 	if username in users:
 		return users.get(username)
 	return None
@@ -38,18 +51,19 @@ def login():
 	if request.method == 'POST':
 		return make_response(jsonify({'error':'Success!'}),201)
 	else:
-		return '<ContentType: text/html> <!DOCTYPE html><html> <head> <title>FlaskBlog Login Page</title> </head> <body> <h1> Login </h1> <p>Under Construction</p></body></html>'
+		return make_response(jsonify({'error':'Failed!'}),404)
 
 @app.route('/blog', methods=['GET','POST'])
 def blog():
 	if request.method == 'GET':
 		#this is where we return all of the user's blog posts
 		return 'under construction'
+		auth.username
 	if request.method == 'POST':
 		#this is where we create a new post
 
 		#for command line:
-		# curl -u jtramley:"password" -H "Content-Type: application/json"  -X POST -d '{"username":"jtramley", "title":"my first post", "text":"So this is my first blog "}' http://srv:5000/blog
+		# curl -u jtramley:"password" -H "Content-Type: application/json"  -X POST -d '{"username":"jtramley", "title":"my first post", "text":"So this is my first blog "}' http://localhost:5000/blog
 
 		username = request.json.get('username',"")
 		title = request.json.get('title',"")
@@ -57,5 +71,9 @@ def blog():
 		return text
 
 	return 'under construction'
+
+@app.route('/blog/<string:username>/<int:page>')
+def getEntries:
+	
 if __name__ == '__main__':
-    app.run(port=5000, host='192.168.2.23')
+    app.run(port=5000, host='localhost')
